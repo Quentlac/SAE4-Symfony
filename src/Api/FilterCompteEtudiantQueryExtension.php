c17b2abf235013954a07a99e87f3aad52936d89d
PhM38 <philippe.martin1@univ-grenoble-alpes.fr>
2023-01-25 12:12:35 +0100

    Modif Filtre API pour qu'un ADMIN voit toutes les ressources

--- src/Api/FilterCompteEtudiantQueryExtension.php
+++ src/Api/FilterCompteEtudiantQueryExtension.php
@@ -17,38 +17,44 @@ class FilterCompteEtudiantQueryExtension implements QueryCollectionExtensionInte
 
     private int $idCompte;   // id du compte de l'étudiant connecté
     private int $idEtudiant; // id de l'étudiant connecté
+    private bool $isAdmin;   // l'étudiant connecté a-t-il le ROLE_ADMIN
 
     // On récupère le compte de l'utilisateur 
     //   pour n'afficher que les données qui le concernent
     public function __construct(Security $security) {
         $this->idCompte = $security->getUser()->getId();
         $this->idEtudiant = $security->getUser()->getEtudiant()->getId();
+        $this->isAdmin = $security->isGranted('ROLE_ADMIN');
     }
 
     public function applyToCollection(QueryBuilder $qb, QueryNameGeneratorInterface $queryNameGenerator,
             string $resourceClass, $operationName = null) {
-        if (Etudiant::class === $resourceClass) {
-            $qb->andWhere(sprintf("%s.id = %d", $qb->getRootAliases()[0], $this->idEtudiant));
-        } elseif (CompteEtudiant::class === $resourceClass) {
-            $qb->andWhere(sprintf("%s.id = %d", $qb->getRootAliases()[0], $this->idCompte));
-        } elseif (OffreConsultee::class === $resourceClass ||
-                OffreRetenue::class === $resourceClass ||
-                Candidature::class === $resourceClass) {
-            $qb->andWhere(sprintf("%s.compteEtudiant = %d", $qb->getRootAliases()[0], $this->idCompte));
+        if (!$this->isAdmin) {
+            if (Etudiant::class === $resourceClass) {
+                $qb->andWhere(sprintf("%s.id = %d", $qb->getRootAliases()[0], $this->idEtudiant));
+            } elseif (CompteEtudiant::class === $resourceClass) {
+                $qb->andWhere(sprintf("%s.id = %d", $qb->getRootAliases()[0], $this->idCompte));
+            } elseif (OffreConsultee::class === $resourceClass ||
+                    OffreRetenue::class === $resourceClass ||
+                    Candidature::class === $resourceClass) {
+                $qb->andWhere(sprintf("%s.compteEtudiant = %d", $qb->getRootAliases()[0], $this->idCompte));
+            }
         }
     }
 
     public function applyToItem(QueryBuilder $qb, QueryNameGeneratorInterface $queryNameGenerator,
             string $resourceClass, array $identifiers, $operationName = null,
             array $context = []) {
-        if (Etudiant::class === $resourceClass) {
-            $qb->andWhere(sprintf("%s.id = %d", $qb->getRootAliases()[0], $this->idEtudiant));
-        } elseif (CompteEtudiant::class === $resourceClass) {
-            $qb->andWhere(sprintf("%s.id = %d", $qb->getRootAliases()[0], $this->idCompte));
-        } elseif (OffreConsultee::class === $resourceClass ||
-                OffreRetenue::class === $resourceClass ||
-                Candidature::class === $resourceClass) {
-            $qb->andWhere(sprintf("%s.compteEtudiant = %d", $qb->getRootAliases()[0], $this->idCompte));
+        if (!$this->isAdmin) {
+            if (Etudiant::class === $resourceClass) {
+                $qb->andWhere(sprintf("%s.id = %d", $qb->getRootAliases()[0], $this->idEtudiant));
+            } elseif (CompteEtudiant::class === $resourceClass) {
+                $qb->andWhere(sprintf("%s.id = %d", $qb->getRootAliases()[0], $this->idCompte));
+            } elseif (OffreConsultee::class === $resourceClass ||
+                    OffreRetenue::class === $resourceClass ||
+                    Candidature::class === $resourceClass) {
+                $qb->andWhere(sprintf("%s.compteEtudiant = %d", $qb->getRootAliases()[0], $this->idCompte));
+            }
         }
     }
 
