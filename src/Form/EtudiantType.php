6f546341a2da90b3a4366db813cca1d3c31abab2
niemazm <milo.niemaz@gmail.com>
2023-03-29 16:30:52 +0200

    Gestion des erreurs des formulaires OffreType.php et EtudiantType.php

--- src/Form/EtudiantType.php
+++ src/Form/EtudiantType.php
@@ -6,16 +6,64 @@ use App\Entity\Etudiant;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Regex;
+
 
 class EtudiantType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('numeroINE')
-            ->add('nom')
-            ->add('prenom')
-            ->add('email')
+            ->add('numeroINE', null, [
+                'label' => 'Numéro INE',
+                'attr' => [
+                    'placeholder' => 'Exemple : 1234567890A',
+                ],
+                'constraints' => [
+                    new Regex([
+                        'pattern' => '/^[0-9]{10}[A-Z]$/',
+                        'message' => 'Le numéro INE doit être composé de 10 chiffres et une lettre majuscule.',
+                    ]),
+                ],
+            ])
+            ->add('nom', null, [
+                'attr' => [
+                    'placeholder' => 'Exemple : DUPONT',
+                ],
+                'constraints' => [
+                    new Regex([
+                        // le nom doit etre composé de lettres majuscules (avec un tiret en cas de nom composé)
+                        'pattern' => '/^[A-Z]+(-[A-Z]+)?$/',
+                        'message' => 'Le nom doit être composé de lettres majuscules.',
+                    ])
+                ],
+            ])
+            ->add('prenom', null, [
+                'attr' => [
+                    'placeholder' => 'Exemple : Jean',
+                ],
+                'constraints' => [
+                    new Regex([
+                        // Le prénom doit commencer par une majuscule et être composé uniquement de lettres
+                        // ou, pour les prénoms composés, d'une majuscule suivi de lettres minuscules puis d'un tiret et d'une majuscule suivi de lettres minuscules
+                        'pattern' => '/^[A-Z][a-z]+(-[A-Z][a-z]+)?$/',
+                        'message' => 'Le prénom doit commencer par une majuscule et être composé uniquement de lettres.',
+                    ])
+                ],
+            ])
+            ->add('email', null, [
+                'attr' => [
+                    'placeholder' => 'Exemple : exemple@iut2.fr',
+                ],
+                'constraints' => [
+                    new Regex([
+                        // l'email doit commencer par une ou plusieurs caractères (avec un .,un - ou un _), d'un arrobase, d'un ou plusieurs caractères, d'un point, d'un ou plusieurs caractères
+                        'pattern' => '/^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+$/',
+                        'message' => 'L\'email doit être composé d\'un ou plusieurs caractères, d\'un arrobase, d\'un point et d\'un ou plusieurs caractères.',
+                    ])
+                ],
+            ])
+
         ;
     }
 
--- src/Form/OffreType.php
+++ src/Form/OffreType.php
@@ -4,10 +4,15 @@ namespace App\Form;
 
 use App\Entity\Offre;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Validator\Constraints\LessThan;
+use Symfony\Component\Validator\Constraints\LessThanOrEqual;
+use Symfony\Component\Validator\Constraints\Url;
+use Symfony\Component\Validator\Constraints\GreaterThan;
 
 class OffreType extends AbstractType {
 
@@ -16,10 +21,29 @@ class OffreType extends AbstractType {
                 ->add('entreprise')
                 ->add('intitule')
                 ->add('descriptif')
-                ->add('dateDepot', null, ["widget" => "single_text"])
+                // la date de dépot doit etre inferieure ou egale a la date du jour
+                ->add('dateDepot', DateType::class, [
+                    'widget' => 'single_text',
+                    'constraints' => [
+                        new LessThanOrEqual(['value' => new \DateTime(), 'message' => 'La date de dépot doit être inférieure ou égale à la date du jour.'])
+                    ]
+                ])
                 ->add('parcours', ChoiceType::class, ["choices" => ["Indéfini" => "*", "Parcours A" => "A", "Parcours B" => "B"]])
-                ->add('motsCles')
-                ->add('urlPieceJointe')
+                ->add('motsCles', null, [
+                    'attr' => [
+                        'placeholder' => 'Exemple : Java; Symfony; Angular',
+                    ],
+                    'help' => 'Séparez les mots clés par un point-virgule.'
+                ])
+                ->add('urlPieceJointe', null, [
+                    'constraints' => [
+                        new Url(['message' => 'L\'url n\'est pas valide.'])
+                    ],
+                    'attr' => [
+                        'placeholder' => 'Exemple : https://exemple',
+                    ],
+
+                ])
                 ->add('etatOffre')
 
         ;
